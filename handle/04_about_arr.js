4b5e72ed7cdece3cfd24558ad08105669174e03b
storm <ningyou123@qq.com>
2022-01-10 12:32:42 +0800

    --数组扁平化/flat/forEach/reduce三种实现方式

--- handle/04_about_arr.js
+++ handle/04_about_arr.js
@@ -14,4 +14,53 @@ function noRepeat (arr) {
 let arr = [11,2,4,9,'22','sjdh',2,4]
 console.log(noRepeat(arr))
 
-// 
+// 数组扁平化 最简单实用的
+function  flat (arr) {
+  return arr.flat(Infinity)
+}
+// console.log(flat([[[[1]]],2,3,[[[[[[8]]]]]]]))
+
+// 先用for循环实现下
+function flat (arr) {
+  let newArr = []
+  function fn (arr) {
+    if(arr instanceof Array){
+      arr.forEach(fn)
+    }else{
+      return newArr.push(arr)
+    }
+  }
+  fn(arr)
+  return newArr
+}
+
+console.log(flat([[[[1]]],2,3,[[[[[[8]]]]]]]))
+console.log(flat([1, 2, [1, [2, 3, [4, 5, [6]]]]]));
+
+
+// 用reduce方法实现
+function flat (arr) {
+  return arr.reduce(
+    (pre,cur)=>{  
+      if(cur instanceof Array){
+        pre = [...pre,...flat(cur)]
+      }else{
+        pre.push(cur)
+      }
+      return pre
+    },
+    []
+  )
+}
+
+// 这个思路===运用扩展运算符来实现  还是很巧妙的
+function flatter(arr) {
+  if (!arr.length) return;
+  return arr.reduce(
+    (pre, cur) =>
+      Array.isArray(cur) ? [...pre, ...flatter(cur)] : [...pre, cur],
+    []
+  );
+}
+console.log(flat([1, 2, [1, [2, 3, [4, 5, [6]]]]]));
+
