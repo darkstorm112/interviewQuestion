7beda637a1451575682ee20ec3d267c407b13533
storm <ningyou123@qq.com>
2022-01-14 11:43:11 +0800

    还是可以的嘛

--- handle/11_deepClone.js
+++ handle/11_deepClone.js
@@ -2,9 +2,7 @@
 // 深度克隆
 function deepClone (data) {
   let obj = null
-  if(data instanceof Function){
-    return data
-  }else if(data instanceof Array) {
+  if(data instanceof Array) {
     obj = []
     data.forEach(item=>{
       obj.push(deepClone(item))
@@ -29,31 +27,31 @@ var obj1 = {
 };
 var obj2 = deepClone(obj1);
 console.log(obj1)
-console.log(obj1===obj2)
+console.log(obj1.b===obj2.b)
 
 
 // 深拷贝（考虑到复制 Symbol 类型）
-function isObject(val) {
-  return typeof val === "object" && val !== null;
-}
+// function isObject(val) {
+//   return typeof val === "object" && val !== null;
+// }
 
-function deepClone(obj, hash = new WeakMap()) {
-  if (!isObject(obj)) return obj;
-  if (hash.has(obj)) {
-    return hash.get(obj);
-  }
-  let target = Array.isArray(obj) ? [] : {};
-  hash.set(obj, target);
-  Reflect.ownKeys(obj).forEach((item) => {
-    if (isObject(obj[item])) {
-      target[item] = deepClone(obj[item], hash);
-    } else {
-      target[item] = obj[item];
-    }
-  });
+// function deepClone(obj, hash = new WeakMap()) {
+//   if (!isObject(obj)) return obj;
+//   if (hash.has(obj)) {
+//     return hash.get(obj);
+//   }
+//   let target = Array.isArray(obj) ? [] : {};
+//   hash.set(obj, target);
+//   Reflect.ownKeys(obj).forEach((item) => {
+//     if (isObject(obj[item])) {
+//       target[item] = deepClone(obj[item], hash);
+//     } else {
+//       target[item] = obj[item];
+//     }
+//   });
 
-  return target;
-}
+//   return target;
+// }
 
 // var obj1 = {
 // a:1,
@@ -65,35 +63,36 @@ function deepClone(obj, hash = new WeakMap()) {
 
 
 // 深度拷贝
-function deepClone(obj) {
-  const rootObj = {} // 记录新的对象
-  const cache = [obj] // 引用对象数组
-  const cacheNewValues = [rootObj] // 记录对应引用的新值
+// function deepClone(obj) {
+//   const rootObj = {} // 记录新的对象
+//   const cache = [obj] // 引用对象数组
+//   const cacheNewValues = [rootObj] // 记录对应引用的新值
 
-  const assign = (innerObj) => {
-    const newObj = innerObj === obj ? rootObj : {}
-    Object.keys(innerObj).forEach(key => {
-      const value = innerObj[key]
-      const index = cache.indexOf(value)
-      // 未缓存
-      if (index === -1) {
-        const type = Object.prototype.toString.call(value)
-        if (typeof type === 'object') {
-          newObj[key] = assign(value)
-          // 记录引用的对象以及对应引用的新值
-          cache.push(value)
-          cacheNewValues.push(newObj[key])
-        } else {
-          newObj[key] = value
-        }
-      } else {
-        // 已缓存，取对应缓存的新值
-        newObj[key] = cacheNewValues[index]
-      }
-    })
+//   const assign = (innerObj) => {
+//     // TODO这个判断不等是什么时候会发生？
+//     const newObj = innerObj === obj ? rootObj : {}
+//     Object.keys(innerObj).forEach(key => {
+//       const value = innerObj[key]
+//       const index = cache.indexOf(value)
+//       // 未缓存
+//       if (index === -1) {
+//         const type = Object.prototype.toString.call(value)
+//         if (typeof type === 'object') {
+//           newObj[key] = assign(value)
+//           // 记录引用的对象以及对应引用的新值
+//           cache.push(value)
+//           cacheNewValues.push(newObj[key])
+//         } else {
+//           newObj[key] = value
+//         }
+//       } else {
+//         // 已缓存，取对应缓存的新值
+//         newObj[key] = cacheNewValues[index]
+//       }
+//     })
 
-    return newObj
-  }
+//     return newObj
+//   }
 
-  return assign(obj);
-}
+//   return assign(obj);
+// }
