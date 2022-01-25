fb0596b30f774a0557e50de4959d6bea73710311
storm <ningyou123@qq.com>
2022-01-25 13:40:04 +0800

    --扩展思考

--- handle/26_DOM2JSON.js
+++ handle/26_DOM2JSON.js
@@ -49,3 +49,10 @@ function dom2Json(domtree) {
   domtree.childNodes.forEach((child) => obj.children.push(dom2Json(child)));
   return obj;
 }
+
+// 扩展思考:如果给定的不是一个 Dom 树结构 而是一段 html 字符串 该如何解析?
+
+// 那么这个问题就类似 Vue 的模板编译原理 我们可以利用正则 匹配 html 字符串 
+// 遇到开始标签 结束标签和文本 解析完毕之后生成对应的 ast 并建立相应的父子关联 
+// 不断的 advance 截取剩余的字符串 直到 html 全部解析完毕
+
